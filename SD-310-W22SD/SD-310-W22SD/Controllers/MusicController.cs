2751fa24245e030716e6b9952d51f59552a36fd0
Vince Roman <vince.aragorn.roman@gmail.com>
2022-07-18 00:28:26 -0500

    add music/index and music/artist

---
+++ SD-310-W22SD/SD-310-W22SD/Controllers/MusicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SD_310_W22SD.Controllers
+{
+    public class MusicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
